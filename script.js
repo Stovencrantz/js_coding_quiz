e2303b0fdd1cfd1a56b242eb96325cb03570c474
Stovencrantz <sknapp143@gmail.com>
2025-02-02 12:16:14 -0500

    Updated javascript structures

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const problemGroupEl = document.getElementById("problem_group");
-const startBtnEl = document.getElementById("start_btn");
+const minutesDisplayEl = document.getElementById("minutes");
+const secondsDisplayEl = document.getElementById("seconds");
+const scoreEl = document.getElementById("scoreVal");
+const startBtnEl = document.getElementById("startBtn");
+const questionEl = document.getElementById("question"); //get current question element
+const answerEl = document.getElementById("answer"); // get value of submitted form
+const submitBtn = document.getElementById("submitBtn");
 
 const questions = [
   {
@@ -43,27 +49,102 @@ const questions = [
     "answer": true
   }
 ]
-let problems = [];
-const Problem = {
+
+let index;
+let time = 1;
+let totalSeconds=0;
+let secondsElapsed=0;
+
+const problem = {
   question: "",
   answer: "",
   render: function() {
-
+    this.question = problems[i].question;
+    questionEl.textContent = ""; //questionEl.textContent will equal the current question in our array
   },
   validate: function() {
+    //validate the users answer to the corresponding answer in the question array
 
   }
+}
+
+const score = {
+  scoreVal: 0,
+  update: function() {
+    this.scoreVal++
+  },
+  render: function() {
+    scoreEl.textContent = this.scoreVal;
+  }
+}
+
+function calculateTime(min) {
+  totalSeconds = min*60;
+}
+
+function renderTime(min,sec) {
+  if(min<10) {
+    min = `0${min}`;
+  }
+  if(sec<10) {
+    sec = `0${sec}`
+  }
+  minutesDisplayEl.textContent = min;
+  secondsDisplayEl.textContent = sec;
+}
+
+function renderProblem(index) {
+  let questionTotal = questions.length;
+
 }
 
 function startQuiz() {
   //Do some quiz stuff
-  for(let i = 0; i < questions.length; i++) {
-    Problem.question = questions[i].question;
-    Problem.answer = questions[i].answer;
-    problems.push({Problem.question, Problem.answer});                                                                                                                                                                                                                                                                                                                                                                                                                  
+  if(localStorage.getItem("sk_score")) {
+    scoreVal = localStorage.getItem("sk_score");
+    scoreEl.textContent = scoreVal;
   }
+  //begin timeinterval and include functions here. 
+  calculateTime(time);
+  secondsElapsed++;
+  let minutes;
+  let seconds;
+  // console.trace(timer.secondsElapsed)
+  // console.trace(timer.totalSeconds)
+  console.trace("totalSeconds: ", totalSeconds)
+  renderProblem();
+  interval = setInterval(function() {
+    minutes = Math.floor((totalSeconds-secondsElapsed)/60)
+    seconds = Math.floor((totalSeconds-secondsElapsed)%60);
+    console.log("Minutes: " + minutes + "|| Seconds: " + seconds);
+    renderTime(minutes,seconds);
+    if(secondsElapsed >= totalSeconds) {
+      clearInterval(interval)
+    }
+    secondsElapsed++;
+
+  }, 1000)
 }
 
-document.addEventListener("click", (event) => {
+startBtnEl.addEventListener("click", (event) => {
   startQuiz();
+  startBtnEl.setAttribute("disabled", '');
 })
+submitBtn.addEventListener("click", (event) => {
+  document.preventDefault();
+  problems.validate();
+  renderProblem(i);
+})
+
+//WHEN startQuiz clicked
+  // THEN begin countdown
+  // THEN render question
+// WHEN question submitted
+  // THEN validate user answer against known answer
+// WHEN user answer==known answer\
+  //THEN  increment scoreVal
+// WHEN user completes all questions OR timer reaches 0
+  // THEN render score
+// update score in localStorage
+// pull value from localStorage on page load
+ 
